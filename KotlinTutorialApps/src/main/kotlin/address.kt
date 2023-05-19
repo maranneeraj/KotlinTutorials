fbdc4b07e8a58caa838c889bfec1e90269ead0ec
NEERAJ MARAN <maranneeraj@gmail.com>
2023-05-19 08:53:24 +0530

    kotlin tutorials new

---
+++ KotlinTutorialApps/src/main/kotlin/address.kt
@@ -0,0 +1,24 @@
+class Address {
+    var name: String = "Holmes, Sherlock"
+    var street: String = "Baker"
+    var city: String = "London"
+    var state: String? = null
+    var zip: String = "123456"
+
+    fun copyAddress(address: Address): Address {
+        val result = Address() // there's no 'new' keyword in Kotlin
+        result.name = address.name // accessors are called
+        result.street = address.street
+        print(result.name)
+        return result
+
+    }
+}
+
+
+
+fun main() {
+val address = Address()
+    address.copyAddress(address)
+    print(address.copyAddress())
+}
