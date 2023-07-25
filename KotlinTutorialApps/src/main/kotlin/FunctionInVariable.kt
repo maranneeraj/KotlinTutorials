0d1333b355a57292b2c61d62ca16e8a29c159cba
NEERAJ MARAN <maranneeraj@gmail.com>
2023-07-25 08:43:07 +0530

    Kotlin tutorial

---
+++ KotlinTutorialApps/src/main/kotlin/FunctionInVariable.kt
@@ -0,0 +1,16 @@
+
+
+fun main() {
+
+    val trafficlight = "Red"
+    if(trafficlight=="Green"){
+        println("Go")
+    }
+    else if(trafficlight=="Yellow"){
+        println("Go Slow")
+    }
+    else{
+        println("STOP")
+    }
+
+}
